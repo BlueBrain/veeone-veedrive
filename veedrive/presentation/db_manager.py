7e4b5bfaa3d85c83a042a2fcca220c0e60fdd989
Pawel Jozef Podhajski <pawel.podhajski@epfl.ch>
2022-08-26 09:07:05 +0200

    Remove Mongodb support

--- veedrive/presentation/db_manager.py
+++ veedrive/presentation/db_manager.py
@@ -1,6 +1,3 @@
-from veedrive.config import DB_TYPE
-
-from .mongo_connector import MongoConnector
 from .pg_connector import create_pg_connector
 
 db = None
@@ -10,11 +7,4 @@ async def get_db():
     global db
     if db:
         return db
-    db = (
-        MongoConnector()
-        if DB_TYPE == "mongo"
-        else await create_pg_connector()
-        if DB_TYPE == "postgres"
-        else None
-    )
-    return db
+    return await create_pg_connector()
